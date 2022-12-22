7b511ca668084a4082a29c658c0696390233d390
Axel Andersson <86422959+axel99andersson@users.noreply.github.com>
2022-12-22 13:55:19 +0100

    Delete alt_hamilt.py

--- graveyard/alt_hamilt.py
+++
@@ -1,7 +0,0 @@
-import torch.sparse
-
-for k in range(n):
-    for l in range(n):
-        for i in range(n-1):
-            W = G.get_edge_data(k, l)['weight']
-            J = torch.sparse.zeros(size = [n, n, n, n] requires_grad=True)
